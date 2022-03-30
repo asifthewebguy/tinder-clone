164e89045c2538ccd809b7bd04d570366d6f7151
Mohammad Asif Ullah Chowdhury <asifthewebguy@gmail.com>
2022-03-30 20:52:26 +0600

    updated

--- client/src/components/ChatInput.js
+++ client/src/components/ChatInput.js
@@ -1,6 +1,28 @@
-import {useState} from 'react'
-const ChatInput = () => {
-const [textArea, setTextArea] = useState(null);
+import {useState} from 'react';
+import axios from 'axios';
+const ChatInput = ({user, clickedUser, getUsersMessages, getClickedUserMessages}) => {
+    const [textArea, setTextArea] = useState(null);
+    const userId = user?.user_id;
+    const clickedUserId = clickedUser?.user_id;
+
+    const sendMessage = async () => {
+        const message = {
+            timestamp: new Date.toISOString(),
+            from_userId: userId,
+            to_userId: clickedUserId,
+            message: textArea
+        };
+
+        try {
+            await axios.post('http://localhost:8000/messages', message);
+            getUsersMessages();
+            getClickedUserMessages();
+            setTextArea('');
+        } catch (err){
+            console.log(err);
+        }
+    }
+
     return (
         <div className="chat-input">
            <textarea
@@ -9,7 +31,7 @@ const [textArea, setTextArea] = useState(null);
                 value={textArea === null ? '' : textArea}
                 onChange={(e)=>{setTextArea(e.target.value)}}>
             </textarea>
-           <button className="secondary-button">Submit</button>
+           <button className="secondary-button" onClick={sendMessage}>Submit</button>
         </div>
     );
 }
