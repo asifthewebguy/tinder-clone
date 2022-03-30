c9b2d50b5040978e7435d08057a95ca7153c70f8
Mohammad Asif Ullah Chowdhury <asifthewebguy@gmail.com>
2022-03-30 20:18:01 +0600

    updated

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,7 +1,18 @@
-const Chat = () => {
+const Chat = ({descendingOrderMessages}) => {
     return (
         <div>
             <div className="chat-display">
+                {descendingOrderMessages?.map((message, _index) => (
+                    <div key={message._index}>
+                        <div className="chat-message-header">
+                            <div className="img-container">
+                                <img src={message.img} alt={message.first_name + ' profile'} />
+                            </div>
+                            <p className="message-name">{message.name}</p>
+                        </div>
+                        <p className="message-text">{message.message}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
